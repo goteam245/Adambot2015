5962672c10f6cf03691b7a8a27e61cd8331cc520
Matt Shaya <mshaya@me.com>
2015-01-24 14:11:00 -0500

    Changed to Interior

--- src/Interior/Interior.java
+++ src/Interior/Interior.java
@@ -1,23 +1,57 @@
 package Interior;
 
 public class Interior {
-	/**
-	 * 
-	 * @param speed set the motor to this speed also do some current checking
-	 */
-	public static void toggleRollers(int speed){
-		
+/**
+*
+* @param speed set the motor to this speed also do some current checking
+*/
+
+	public static void toggleRollers(int speed){ 
+	
 	}
+	
 	//sets the motor speeds of both arms
 	public static void moveArm(double speed){
 		
 	}
-	//clamps using the piston
-	public static void clamp(boolean isCompressed){
-		
+	
+	public static void toggleArm (boolean isCompressed) {
+		//true = closed
+		//false = open
+		if (isCompressed) {
+			//open Arms
+		} else {
+			//close Arms
+		}
 	}
+	
+	//clamps using the piston
+	public static void toggleClamps(boolean isCompressed){
+		//true = closed
+		//false = open
+		if (isCompressed) {
+			//open clamps
+		} else {
+			//close clamps
+		}
+	}
+	
 	//releases the crates
 	public static void release(){
+		//need mechanism info
+	}
+	
+	public static void liftAndLock () {
+		//close arms
+		//move arms up
+		//close clamps
+		//open arms
+		//move arms down
+	}
+	
+	public static void currentCheck () {
 		
 	}
+	
 }
+
